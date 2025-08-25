84c53412175cc46a099c2d907728ac3c9239ba36
Hao079231 <haotrinh2003.com@gmail.com>
2025-08-25 23:00:32 +0700

    trunghao: lam phan export cho FE

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const accountRouter = require('./route/accountRouter');
 const permissionRouter = require('./route/permissionRouter');
 const groupRouter = require('./route/groupRouter');
 const sequelize = require('./config/dbConfig');
+const cors = require('cors');
 
 //Config use port and hostname from .env
 const PORT = process.env.PORT || 8181;
@@ -14,6 +15,34 @@ const HOST_NAME = process.env.HOST_NAME || 'localhost';
 
 //Config app use json
 app.use(express.json());
+
+// middleware for CORS
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:4000']; // cho PROD
+let corsOptions;
+
+if (process.env.NODE_ENV === 'production') {
+  // Chỉ cho phép origin nằm trong danh sách
+  corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // cho Postman
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `CORS policy does not allow access from origin ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true
+  };
+} else {
+  // DEV: cho phép tất cả
+  corsOptions = {
+    origin: '*',
+    credentials: true
+  };
+}
+
+app.use(cors(corsOptions));
+
 
 // Use file api
 app.use('/v1', studentRouter);
